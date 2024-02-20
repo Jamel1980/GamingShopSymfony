c40b53dba5d814d8e001fc454e2842ba54ed5efb
Mabook Air <mabookair@macbook-air-de-mabook.home>
2024-02-20 14:18:32 +0100

    sauvegardeprojet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,8 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        
+        
         ]);
     }
 }

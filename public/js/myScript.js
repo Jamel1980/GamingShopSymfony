3eedf8e13d4a1d1d45826db208c94eee228b4d7d
tchering <sonam.sherpa.lama@gmail.com>
2024-02-21 16:52:48 +0100

    button

---
+++ public/js/myScript.js
@@ -0,0 +1,26 @@
+function getIdChecked(name_element) {
+  let checkboxes = document.getElementsByName(name_element);
+  let id = 0;
+  checkboxes.forEach((item) => {
+    if (item.checked == true) {
+      id = item.value;
+      stop;
+    }
+  });
+  return id;
+}
+
+// function onlyOne(checkbox) {
+//     let checkboxes = document.getElementsByName(checkbox.name);
+//     checkboxes.forEach(box => box.checked = (box === checkbox));
+// }
+function onlyOne(checkbox) {
+    let checkboxes = document.getElementsByName(checkbox.name);
+    checkboxes.forEach((box) =>{
+        if (box === checkbox) {
+            box.checked = true;
+        } else {
+            box.checked = false;
+        }
+    });
+}

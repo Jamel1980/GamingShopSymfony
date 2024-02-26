92130696c55924bba0a34076c3219d715c896f31
tchering <sonam.sherpa.lama@gmail.com>
2024-02-26 21:41:19 +0100

    created ClientController

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ClientController extends AbstractController
+{
+    #[Route('/client', name: 'app_client')]
+    public function index(): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+#[Route('/client/edit-photo', name:'app_client_edit_photo')]
+    public function editPhoto(Request $request){
+    }
+}

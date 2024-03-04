16f9e18168b48d1ccee6d79f1bb204b2d477b0b9
tchering <sonam.sherpa.lama@gmail.com>
2024-03-04 17:07:14 +0100

    completed upload controller

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -18,6 +18,7 @@ class ClientController extends AbstractController
 {
     #[Route('/client', name: 'app_client')]
 
+
     public function index(ClientRepository $cr, EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
@@ -27,10 +28,10 @@ class ClientController extends AbstractController
             'client' => $client, // Pass the client to the template
             'user' => $user,
             'photo' => $photo,
-
         ]);
     }
     #[Route('/client/edit-photo', name: 'app_client_edit_photo')]
+    //! this function edit is using method without creating form object. So method is bit different.
     public function edit(Request $request, ClientRepository $clientRepository, EntityManagerInterface $em): Response
     {
         $user = $this->getUser(); // Get the logged-in user
@@ -47,6 +48,8 @@ class ClientController extends AbstractController
             $client->setNom($form['nom']);
             $client->setPrenom($form['prenom']);
             $client->getUser()->setEmail($form['email']);
+            // $nom = $form->get('nom')->getData();
+            // $client->setNom($nom);
             $client->getUser()->setPassword($form['password']);
 
             // Handle the uploaded photo
@@ -79,4 +82,50 @@ class ClientController extends AbstractController
             'client' => $client, // Pass the client to the template
         ]);
     }
+
+    //! here the uploading and modifying client data is handled using form object.
+    //     public function edit(Request $request, ClientRepository $clientRepository, EntityManagerInterface $em): Response
+    // {
+    //     $user = $this->getUser(); // Get the logged-in user
+
+    //     $client = $clientRepository->findOneBy(['user' => $user]); // Get the client associated with the user
+
+    //     $form = $this->createForm(ClientType::class, $client);
+
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         // Handle the form submission
+
+    //         // Handle the uploaded photo
+    //         $photo = $form->get('photo')->getData();
+    //         if ($photo) {
+    //             $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+    //             $newFilename = $originalFilename . '-' . uniqid() . '.' . $photo->guessExtension();
+
+    //             try {
+    //                 $photo->move(
+    //                     $this->getParameter('upload_directory'), // This should be defined in your config/services.yaml file
+    //                     $newFilename
+    //                 );
+    //             } catch (FileException $e) {
+    //                 // Handle exception if something happens during file upload
+    //             }
+
+    //             $client->setPhoto($newFilename);
+    //         }
+
+    //         // Save the changes to the database
+    //         $em->persist($client);
+    //         $em->flush();
+
+    //         // Redirect the user to a success page
+    //         return $this->redirectToRoute('app_client_success');
+    //     }
+
+    //     return $this->render('client/edit.html.twig', [
+    //         'client' => $client, // Pass the client to the template
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 }

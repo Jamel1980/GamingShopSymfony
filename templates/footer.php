cd485813c4e66547530fd52d24d7a891036d6fb7
Mabook Air <mabookair@pc-199.home>
2024-03-07 15:32:57 +0100

    modification

--- templates/footer.php
+++ templates/footer.php
@@ -5,20 +5,20 @@
 <footer>
     <div class="row bg-dark justify-content-around custom-line ">
         <div class="col-3 footer-section text-light my-4">
-          <h5 class="text-center text-primary Larger shadow my-2">CATÉGORIES</h5>
-          <ul class="text-center">
-            <li class="footer-li"><a href="index.php">JEUX VIDÉO</a></li>
-            <li class="footer-li"><a href="View/page_articles.php">CONSOLE</a></li>
-            <li class="footer-li"><a href="#">ACCESSOIRES</a></li>
-            <li class="footer-li"><a href="#">MOBILIER GAMING</a></li>
-            <li class="footer-li"><a href="#">ESPACE GEEK</a></li>
-            <li class="footer-li"><a href="#">CONTACT</a></li>
-          </ul>
+            <h5 class="text-center text-primary Larger shadow my-2">CATÉGORIES</h5>
+            <ul class="text-center">
+                <li class="footer-li"><a href="index.php">JEUX VIDÉO</a></li>
+                <li class="footer-li"><a href="View/page_articles.php">CONSOLE</a></li>
+                <li class="footer-li"><a href="#">ACCESSOIRES</a></li>
+                <li class="footer-li"><a href="#">MOBILIER GAMING</a></li>
+                <li class="footer-li"><a href="#">ESPACE GEEK</a></li>
+                <li class="footer-li"><a href="#">CONTACT</a></li>
+            </ul>
         </div>
         <div class="col-3 footer-section text-light my-4">
             <h5 class="text-center text-primary Larger shadow my-2"> QUI SOMMES NOUS?</h5>
             <p class="text-center fst-italic ">"Une équipe de développeur web Youen, Sherpa et Jamel passionnées par l'univers jeux vidéos.
-                Nous avons décidés de créer une boutique proposant les tarifs les plus bas du marché 
+                Nous avons décidés de créer une boutique proposant les tarifs les plus bas du marché
                 afin que le jeu reste accessibles à toutes et tous !"
             </p>
         </div>
@@ -26,17 +26,16 @@
             <h5 class="text-primary text-center Larger shadow my-2">CONTACT</h5>
             <p class="text-center">EMAIL : gamingshop@gmail.com</p>
         </div>
-      
-      
+
+
         <div class="row">
             <div class="text-center shadow">
-            <a href="#" target="_blank"><i class="fab fa-facebook-f text-white"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-twitter text-white"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-linkedin-in text-white"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-instagram text-white"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-facebook-f text-white"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter text-white"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-linkedin-in text-white"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram text-white"></i></a>
             </div>
         </div>
         <p class="text-center text-white shadow">&copy; 2024 Gaming SHOP. Tous droits réservés.</p>
     </div>
-</footer> 
-
+</footer>
